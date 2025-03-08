45857461d85a9ad967e628590912abfb84eed73a
BhavikaPathria-02 <path0053@algonquinlive.com>
2025-03-07 21:56:28 -0500

    commit

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "path0053" {
   name     = "path0053-resources"
   location = "East US"
-
+}
 
 resource "azurerm_storage_account" "path0053" {
   name                     = "path0053storacc"
@@ -13,5 +13,4 @@ resource "azurerm_storage_account" "path0053" {
   location                 = azurerm_resource_group.path0053.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-error
+}
